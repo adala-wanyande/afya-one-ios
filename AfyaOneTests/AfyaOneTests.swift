e3a19b5d71116e757d62160a15050d08daf780b5
adala-wanyande <awanyande@gmail.com>
2024-12-10 10:48:31 +0300

    init: first commit

---
+++ AfyaOneTests/AfyaOneTests.swift
@@ -0,0 +1,17 @@
+//
+//  AfyaOneTests.swift
+//  AfyaOneTests
+//
+//  Created by Adala Wanyande on 10/12/2024.
+//
+
+import Testing
+@testable import AfyaOne
+
+struct AfyaOneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
