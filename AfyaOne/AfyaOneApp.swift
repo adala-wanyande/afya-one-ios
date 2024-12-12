7b96b27a41b73c7ee13de07922f48a7a55a503f5
adala-wanyande <awanyande@gmail.com>
2024-12-12 09:20:56 +0300

    chore: integrate firebase sdk

--- AfyaOne/AfyaOneApp.swift
+++ AfyaOne/AfyaOneApp.swift
@@ -6,13 +6,29 @@
 //
 
 import SwiftUI
-import SwiftData
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct YourApp: App {
-    var body: some Scene {
-        WindowGroup {
-            SplashView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
     }
+  }
 }
